b9277f38309d430574f062933e5781e52b7ca212
janotheboss75 <82925930+janotheboss75@users.noreply.github.com>
2026-02-05 00:47:32 -0700

    MEJORA APP

--- Miniweather_ValdezA/app/src/main/java/Valdez/Alejandro/MainActivity.kt
+++ Miniweather_ValdezA/app/src/main/java/Valdez/Alejandro/MainActivity.kt
@@ -1,6 +1,9 @@
 package Valdez.Alejandro
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,12 +22,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //Inicializar los elementos de la UI
+        var index = 0
+        val listaNombresClimas = listOf("cloudy","rainy","snowy","stormy","sunny","windy")
+        val listaIconos = listOf(R.drawable.ic_cloudy, R.drawable.ic_rainy, R.drawable.ic_snowy, R.drawable.ic_stormy, R.drawable.ic_sunny, R.drawable.ic_windy)
+
+
+        //Elementos inicializados
         val tvGreeting = findViewById<TextView>(R.id.tvGreeting)
         val tvCity = findViewById<TextView>(R.id.tvCity)
         val ivWeather = findViewById<ImageView>(R.id.ivWeather)
         val tvTemperature = findViewById<TextView>(R.id.tvTemperature)
         val tvWeather = findViewById<TextView>(R.id.tvWeather)
+        val btnUbicacion = findViewById<Button>(R.id.btnUbicacion)
+        val btnClima = findViewById<Button>(R.id.btnClima)
+
+        tvGreeting.setText("HI, ALEJANDRO!!!")
+        tvCity.setText("China")
+        ivWeather.setImageResource(R.drawable.ic_sunny)
+        tvTemperature.setText("22°")
+        tvWeather.setText("Sunny")
+
+        btnUbicacion.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
+
+        btnClima.setOnClickListener {
+            if(index + 1 < listaIconos.size){
+                index += 1
+            }
+            else{
+                index = 0
+            }
+            tvWeather.setText(listaNombresClimas[index])
+            ivWeather.setImageResource(listaIconos[index])
+        }
+
 
     }
 }
